6ea17dc521bf6fc2196dc962d851b4b31e603605
James Watterson <james@pby-technologies.com>
2023-12-02 19:56:28 -0800

    reformatted day1 with black formatter

--- advent_code_2023/day-01/part1.py
+++ advent_code_2023/day-01/part1.py
@@ -1,18 +1,22 @@
 import os.path
+
+
 def get_left_most_digit(line: str) -> int:
     for char in line:
         if char.isdigit():
             return int(char)
     return -99999999
 
+
 def get_right_most_digit(line: str) -> int:
     reversed = line[::-1]
     return get_left_most_digit(reversed)
 
-path = '~/code/advent-code-2023/advent_code_2023/day-01/input.txt'
+
+path = "~/code/advent-code-2023/advent_code_2023/day-01/input.txt"
 
 sum = 0
-with open(os.path.expanduser(path), 'r') as input_file:
+with open(os.path.expanduser(path), "r") as input_file:
     for line in input_file:
         l = get_left_most_digit(line)
         r = get_right_most_digit(line)
@@ -20,4 +24,4 @@ with open(os.path.expanduser(path), 'r') as input_file:
         sum += calibration_value
 
 print("====================== ATTENTION =======================")
-print(f'The Calibration values add up to: {sum}')
+print(f"The Calibration values add up to: {sum}")
